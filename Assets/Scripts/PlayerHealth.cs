987f434d20b6924e8e64bdd353fb427fd4e80e7f
xHabeS <kkhabas64@gmail.com>
2026-05-14 00:23:46 +0200

    First Enemy & HP

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 1;
+    public int health;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
